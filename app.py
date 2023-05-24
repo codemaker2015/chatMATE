6e4e5e248d2341d529cfbf7a5eefbe8a8b45ece5
Vishnu <codemaker2015@gmail.com>
2023-05-24 18:11:36 +0530

    corrections

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ from hugchat import hugchat
 
 st.set_page_config(page_title="ChatMATE - Chat Multi-purpose AI Technology")
 
-# Generate empty lists for generated and past.
-## generated stores AI generated responses
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["I'm ChatMATE, How may I help you?"]
-## past stores User's questions
-if 'past' not in st.session_state:
-    st.session_state['past'] = ['Hi!']
+# Generate empty lists for bot_response and user_input.
+## bot_response stores AI generated responses
+if 'bot_response' not in st.session_state:
+    st.session_state['bot_response'] = ["I'm ChatMATE, How may I help you?"]
+## user_input stores User's questions
+if 'user_input' not in st.session_state:
+    st.session_state['user_input'] = ['Hi!']
 
 # Layout of input/response containers
 input_container = st.container()
@@ -21,12 +21,12 @@ response_container = st.container()
 
 # User input
 ## Function for taking user provided prompt as input
-def get_text():
+def get_input():
     input_text = st.text_input("You: ", "", key="input")
     return input_text
 ## Applying the user input box
 with input_container:
-    user_input = get_text()
+    user_input = get_input()
 
 # Response output
 ## Function for taking user prompt as input followed by producing AI generated responses
@@ -39,10 +39,10 @@ def generate_response(prompt):
 with response_container:
     if user_input:
         response = generate_response(user_input)
-        st.session_state.past.append(user_input)
-        st.session_state.generated.append(response)
+        st.session_state.user_input.append(user_input)
+        st.session_state.bot_response.append(response)
         
-    if st.session_state['generated']:
-        for i in range(len(st.session_state['generated'])):
-            message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-            message(st.session_state["generated"][i], key=str(i))
+    if st.session_state['bot_response']:
+        for i in range(len(st.session_state['bot_response'])):
+            message(st.session_state['user_input'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state['bot_response'][i], key=str(i))
